482d9e9ecfde6bc8c0a6db843bbf6c88725de033
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:27:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,635 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "The tripartite functions of school library includes these except",
+
+         answer : "Examinations",
+
+         options : [
+
+                          "Community service",
+
+                          "Learning",
+
+                          "Examinations",
+
+                          "Research"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "Presently most University libraries operate __",
+
+         answer : "Hybrid library",
+
+         options : [
+
+                          "Digital library",
+
+                          "Hybrid library",
+
+                          "AI library",
+
+                          "Manual library"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "justifications for libraries include these except",
+
+         answer : "Examinations",
+
+         options : [
+
+                          "Assignments",
+
+                          "Examinations",
+
+                          "Personal development",
+
+                          "Research"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "__ has to do with enquiry into the unknown",
+
+         answer : "Research",
+
+         options : [
+
+                          "Reading",
+
+                          "Learning",
+
+                          "Assignment",
+
+                          "Research"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "Information resources in the library include the following except",
+
+         answer : "Business correspondence",
+
+         options : [
+
+                          "Government publications",
+
+                          "Business correspondence",
+
+                          "Reference information resources",
+
+                          "Library catalogue"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "___ is a systematic listing of materials in the library",
+
+         answer : "Catalogue",
+
+         options : [
+
+                          "Gazettes",
+
+                          "Catalogue",
+
+                          "Journals",
+
+                          "Serials"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "___ resources provide information on persons",
+
+         answer : "Biographical reference",
+
+         options : [
+
+                          "Encyclopedias",
+
+                          "Directories",
+
+                          "Reference resources",
+
+                          "Biographical reference"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "___are usually consulted for specific information Reference",
+
+         answer : "Reference resources",
+
+         options : [
+
+                          "Reference resources",
+
+                          "Encyclopedias",
+
+                          "Directories",
+
+                          "Biological references"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "___ are the most consulted the",
+
+         answer : "",
+
+         options : [
+
+                          "reference books",
+
+                          "Directories",
+
+                          "Encyclopedias",
+
+                          "Bibliographies Yearbooks"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "__ The first step to a successful research is",
+
+         answer : "Knowledge of resources to look at",
+
+         options : [
+
+                          "Knowledge of resources to look at",
+
+                          "A well defined purpose",
+
+                          "Preconceived idea of subject matter",
+
+                          "Access to necessary materials"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "___ are the most prolific publishers",
+
+         answer : "Government publications",
+
+         options : [
+
+                          "Journals periodicals",
+
+                          "Serial publications",
+
+                          "Government publications",
+
+                          "Electronic information resources"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "Classifications of government publications include these except",
+
+         answer : "Institutional repositories",
+
+         options : [
+
+                          "Institutional repositories",
+
+                          "Research documents for specialists",
+
+                          "Records of government administration",
+
+                          "Popular sources of information"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "The most detailed records that supports most scientific, legal and historic studies are found in",
+
+         answer : "Serial publications",
+
+         options : [
+
+                          "Journals periodicals",
+
+                          "Government publications",
+
+                          "Electronic information resources",
+
+                          "Serial publications"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:14,
+
+         question: "The essence of newspapers is its",
+
+         answer : "Timelessness",
+
+         options : [
+
+                          "Timelessness",
+
+                          "Details",
+
+                          "Directory function",
+
+                          "Degree of Information"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:15,
+
+         question: "__ is a publication in any medium issued in successive parts bearing numerical or chronological designations and intended to be continued indefinitely",
+
+         answer : "Serials",
+
+         options : [
+
+                          "Directories",
+
+                          "Serials",
+
+                          "Yearbooks",
+
+                          "Catalogue"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:16,
+
+         question: "The most important part of science and technology libraries is",
+
+         answer : "Periodicals",
+
+         options : [
+
+                          "Journals",
+
+                          "Institutional repositories",
+
+                          "Periodicals",
+
+                          "Serials"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:17,
+
+         question: "___ are usually 5 years ahead of books in every subject",
+
+         answer : "Journals",
+
+         options : [
+
+                          "Institutional repositories",
+
+                          "Periodicals",
+
+                          "Journals",
+
+                          "Directories"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:18,
+
+         question: "__ constitute the backbone of the research collection",
+
+         answer : "Serials",
+
+         options : [
+
+                          "Periodicals",
+
+                          "Institutional repositories",
+
+                          "Journals",
+
+                          "Serials"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:19,
+
+         question: "Electronic information resources include these except",
+
+         answer : "Serial publications",
+
+         options : [
+
+                          "Online public access catalogue",
+
+                          "Online databases",
+
+                          "Institutional repositories",
+
+                          "Serial publications"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:20,
+
+         question: "___ are one of the best sources of critical analysis of any decision usually found in law reviews",
+
+         answer : "Case notes",
+
+         options : [
+
+                          "Case notes",
+
+                          "Technical reports",
+
+                          "Special collections",
+
+                          "Memoirs"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:21,
+
+         question: "Scientists hold which of these in high esteem",
+
+         answer : "Technical reports",
+
+         options : [
+
+                          "Special collections",
+
+                          "Memoirs",
+
+                          "Technical reports",
+
+                          "Case notes"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:22,
+
+         question: "___ houses electronic information resources generated within the institutions",
+
+         answer : "Institutional repositories",
+
+         options : [
+
+                          "Serial publications",
+
+                          "Online public access catalogue",
+
+                          "Online databases",
+
+                          "Institutional repositories"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:23,
+
+         question: "___are means of informing the reading public on what have been published in a particular field",
+
+         answer : "Abstract",
+
+         options : [
+
+                          "Index",
+
+                          "Abstract",
+
+                          "Gazettes",
+
+                          "Guide books"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:24,
+
+         question: "___ is a systematically arranged list of materials given enough information about each item to make it easy to be found",
+
+         answer : "Index",
+
+         options : [
+
+                          "Index",
+
+                          "Gazettes",
+
+                          "Guide books",
+
+                          "Abstract"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:25,
+
+         question: "___ contains bibliographic information of a library",
+
+         answer : "Online public access catalogue",
+
+         options : [
+
+                          "Serial publications",
+
+                          "Institutional repositories",
+
+                          "Online public access catalogue",
+
+                          "Online databases"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:26,
+
+         question: "The most logical place to start searching for a book in a library is",
+
+         answer : "Catalogue",
+
+         options : [
+
+                          "Serials",
+
+                          "Directories",
+
+                          "Catalogue",
+
+                          "Yearbooks"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
